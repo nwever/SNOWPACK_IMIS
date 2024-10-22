6124f241282940c7ebb968f891400b235e3c5589
Nander Wever <nander.wever@slf.ch>
2024-10-22 18:07:46 +0200

    Adding a script that can mimic stop-and-go simulations, as done in an operational setting

---
+++ run_stop_and_go.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Setttings
+start_date="2024-09-01"
+end_date="2024-10-10"
+interval=1800			# in seconds, only integers possible
+stn=$1
+
+if [ -z "${stn}" ]; then
+	echo "Provide stn to run as first command line parameter."
+	exit
+fi
+
+# Convert the dates to unix time
+current_date=$(date -d "${start_date}" +%s)
+end_date_seconds=$(date -d "${end_date}" +%s)
+
+n=0
+while [ "$current_date" -le "$end_date_seconds" ]; do
+	d=$(date -d "@${current_date}" +"%Y-%m-%dT%H:%M")
+
+	# Create a proper ini file for stop-and-go simulations: 1) read sno file from current_snow directory, and suppress all meteorological data after end date
+	inifile=$(fgrep ${stn} to_exec.lst | awk '{for(i=1; i<=NF; i++) {if($i=="-c") {print $(i+1)}}}' | awk -F\/ '{print $NF}')
+	inifile_add="./cfgfiles/stop_and_go_${stn}.ini"
+	if (( ${n} > 0 )); then
+		echo "IMPORT_BEFORE = ${inifile}" > ${inifile_add}
+		echo "[InputEditing]" >> ${inifile_add}
+		echo "*::edit1         = EXCLUDE" >> ${inifile_add}
+		echo "*::arg1::params  = *" >> ${inifile_add}
+		echo "*::arg1::when    = ${d} - ${end_date}" >> ${inifile_add}
+		echo "[INPUT]" >> ${inifile_add}
+		echo "SNOWPATH = ./current_snow" >> ${inifile_add}
+	fi
+
+	# Do a few modifications to the execute command.
+	# 1) Set the -e end time correctly
+	#   After the first iteration:
+	# 2) Remove the -b start time. After the first iteration, start time should be determined from the sno file
+	# 3) Replace the ini file with the stop-and-go ini file
+	# 4) Make sure to specify the -r restart option
+	# 5) Append to the logfile (>>)
+	to_exec=$(fgrep ${stn} to_exec.lst | awk -v n=${n} -v d=${d} -v inifile=${inifile_add} '{for(i=1; i<=NF; i++) {if(i>1) {printf " "}; if(n>0 && $i=="-b") {i++} else if($i=="-e") {printf("-e %s", d); i++} else if($i=="-c" && n>0) {printf("-c %s", inifile); i++} else if($i==">" && n>0) {printf "-r >>"} else {printf "%s", $i}}}')
+
+	eval ${to_exec}
+
+	# Advance by interval
+	current_date=$((${current_date} + ${interval}))
+	let n=${n}+1
+done
